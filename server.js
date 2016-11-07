202fdb614f038d7c61f805a42dcc7527da6098b1
Jobin <xyborg23@users.noreply.github.com>
2016-11-07 12:00:46 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-// Get secrets from server environment
-var botConnectorOptions = { 
-    appId: process.env.BOTFRAMEWORK_APPID, 
-    appPassword: process.env.BOTFRAMEWORK_APPSECRET
-};
-
-// Create bot
-var bot = new builder.BotConnectorBot(botConnectorOptions);
-bot.add('/', function (session) {
-    
-    //respond with user's message
-    session.send("You said " + session.message.text);
-});
-
 // Setup Restify Server
 var server = restify.createServer();
 
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: process.env.BOTFRAMEWORK_APPID,
+    appPassword: process.env.BOTFRAMEWORK_APPSECRET
+});
+
+// Create bot
+var bot = new builder.UniversalBot(connector);
 // Handle Bot Framework messages
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.post('/api/messages', connector.listen());
+bot.dialog('/', function (session) {
+    
+    //respond with user's message
+    session.send("You said ");
+});
+
+//server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 
 // Serve a static web page
 server.get(/.*/, restify.serveStatic({
@@ -27,6 +32,3 @@ server.get(/.*/, restify.serveStatic({
 	'default': 'index.html'
 }));
 
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
-});
